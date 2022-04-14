94836124e82d101781cf29e2ac5779d74b8063aa
tomwang-varicent <lwang@varicent.com>
2022-04-14 17:10:41 -0400

    use tap_stream_id instead of stream

--- tap_mysql/sync_strategies/common.py
+++ tap_mysql/sync_strategies/common.py
@@ -173,7 +173,7 @@ def row_to_singer_record(catalog_entry, version, row, columns, time_extracted):
     rec = dict(zip(columns, row_to_persist))
 
     return singer.RecordMessage(
-        stream=catalog_entry.stream,
+        stream=catalog_entry.tap_stream_id,
         record=rec,
         version=version,
         time_extracted=time_extracted)
